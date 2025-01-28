c3ee8d74427e994071f02b05e77e9120df965c9f
AhmadInsight <ahmad.pk9098@gmail.com>
2025-01-28 22:00:46 +0500

    Update AboutMe.tsx

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -12,12 +12,14 @@ export const AboutMe = () => {
         <div className="col-lg-6 col-xm-12">
           <h1 className="about-heading">About Me</h1>
           <p>
-            I am a full stack web developer from Bahawalpur Pakistan. I am
-            assistant professor at IUB. I started writing code about 19 years
-            ago. I started with Java and then moved to web technologies like
-            HTML, CSS, JavaScript and PHP. I have worked for companies as well
-            as ran my own. I love helping students learn to code and better
-            their lives.
+            I’m Ahmad Hassan, a dedicated software engineer specializing in
+            modern, scalable web apps. Passionate about web innovation,
+            I craft dynamic, responsive websites with seamless front-end
+            experiences and robust backend systems. With expertise in Python,
+            MySQL, and Java, I focus on integrating complex functionalities to
+            deliver efficient, user-friendly solutions. My goal is to leverage
+            the latest technologies to build applications that empower
+            businesses and enhance user engagement.
           </p>
         </div>
       </div>

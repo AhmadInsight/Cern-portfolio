a9de643a62e047ede2f732ccb3efb900a0e0a8ca
AhmadInsight <ahmad.pk9098@gmail.com>
2025-01-28 22:05:22 +0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { FaFacebook, FaTwitter, FaLinkedin } from "react-icons/fa";
-import { FaGoogleScholar } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa6";
 
 export const Footer = () => {
   return (
@@ -12,10 +12,12 @@ export const Footer = () => {
               <p>City Bahawalpur, Punjab, Pakistan</p>
             </div>
             <div className="d-flex flex-column align-items-center mb-3">
-              <a href="tel:+92-313-7754000" className="footer-contact-link">(+92)-313-7754000</a>
+              <a href="tel:+92-328-7903245" className="footer-contact-link">
+                (+92)-328-7903245
+              </a>
             </div>
             <div className="d-flex flex-column align-items-center">
-              <p className="footer-contact-link">waheed@iub.edu.pk</p>
+              <p className="footer-contact-link">ahmad-dev-jsx@gmail.com</p>
             </div>
           </div>
 
@@ -23,14 +25,30 @@ export const Footer = () => {
           <div className="col-lg-3 col-md-4 col-sm-6 mb-4 mb-lg-0">
             <div className="row">
               <div className="col-6">
-                <a href="" className="footer-nav">Home</a><br />
-                <a href="" className="footer-nav">About me</a><br />
-                <a href="" className="footer-nav">Services</a>
+                <a href="#home" className="footer-nav">
+                  Home
+                </a>
+                <br />
+                <a href="#about" className="footer-nav">
+                  About me
+                </a>
+                <br />
+                <a href="#services" className="footer-nav">
+                  Services
+                </a>
               </div>
               <div className="col-6">
-                <a href="" className="footer-nav">Experience</a><br />
-                <a href="" className="footer-nav">Portfolio</a><br />
-                <a href="" className="footer-nav">Contact</a>
+                <a href="" className="footer-nav">
+                  Experience
+                </a>
+              <br />
+                <a href="" className="footer-nav">
+                  Portfolio
+                </a>
+                <br />
+                <a href="#contacts" className="footer-nav">
+                  Contact
+                </a>
               </div>
             </div>
           </div>
@@ -38,21 +56,42 @@ export const Footer = () => {
           {/* Social media links section */}
           <div className="col-lg-5 col-md-4 col-sm-6 text-center mb-4 mb-lg-0">
             <div className="d-flex justify-content-center mb-3">
-              <a href="https://www.facebook.com/waheed.anwer.94" target="_blank" rel="noopener noreferrer" className="footer-icons">
+              <a
+                href="https://www.facebook.com/profile.php?id=100011489012407"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="footer-icons"
+              >
                 <FaFacebook size={30} />
               </a>
-              <a href="https://twitter.com/yourprofile" target="_blank" rel="noopener noreferrer" className="footer-icons">
+              <a
+                href="https://twitter.com/yourprofile"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="footer-icons"
+              >
                 <FaTwitter size={30} />
               </a>
-              <a href="https://www.linkedin.com/in/waheed-anwar-ph-d-95000321" target="_blank" rel="noopener noreferrer" className="footer-icons">
+              <a
+                href="https://www.linkedin.com/in/ahmad-hassan-63195a173/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="footer-icons"
+              >
                 <FaLinkedin size={30} />
               </a>
-              <a href="https://scholar.google.com.pk/citations?user=IPHx6tIAAAAJ&hl=en" target="_blank" rel="noopener noreferrer" className="footer-icons">
-                <FaGoogleScholar size={30} />
+              <a
+                href="https://www.linkedin.com/in/ahmad-hassan-63195a173/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="footer-icons"
+              >
+                <FaGithub size={30} />
               </a>
             </div>
             <p className="pt-3 text-center">
-              copyright&copy; {new Date().getFullYear()} WaheedInsights | All Rights Reserved
+              copyright&copy; {new Date().getFullYear()} AhmadInsights | All
+              Rights Reserved
             </p>
           </div>
         </div>

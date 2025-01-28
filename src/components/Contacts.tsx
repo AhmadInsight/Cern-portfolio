df0ca24f5216e4e9af94e9f9a94dbaa5a3977738
AhmadInsight <ahmad.pk9098@gmail.com>
2025-01-28 22:02:12 +0500

    Update Contacts.tsx

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -18,9 +18,9 @@ const Contacts = () => {
     formState: { errors },
   } = useForm<ContactFormData>(); // Typing the form data
 
-  const YOUR_SERVICE_ID = "service_at4cghg";
-  const YOUR_TEMPLATE_ID = "template_gf69imn";
-  const YOUR_USER_ID = "c6GBUdPakiaAdbjW3";
+  const YOUR_SERVICE_ID = "service_6mz01kf";
+  const YOUR_TEMPLATE_ID = "template_903pbbw";
+  const YOUR_USER_ID = "PCwOBm_-ejT22KU5M";
 
   // Typing the onSubmit function
   const onSubmit = (data: ContactFormData, r: React.BaseSyntheticEvent) => {
